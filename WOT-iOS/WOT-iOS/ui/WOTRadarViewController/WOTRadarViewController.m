4cfe680e5aeb438415733794e828bcefd580847b
Pavel Yeshchyk <paul.yestchick@gmail.com>
2015-06-17 11:20:15 +0300

    no message

--- WOT-iOS/WOT-iOS/ui/WOTRadarViewController/WOTRadarViewController.m
+++ WOT-iOS/WOT-iOS/ui/WOTRadarViewController/WOTRadarViewController.m
@@ -8,12 +8,11 @@
 
 #import "WOTRadarViewController.h"
 #import "WOT_iOS-Swift.h"
+#import "WOTRadarSampleMetric.h"
 
 @interface WOTRadarViewController () <ChartViewDelegate>
 
 @property (nonatomic, strong)IBOutlet RadarChartView *radarView;
-@property (nonatomic, strong)NSArray *options;
-@property (nonatomic, strong)NSArray *parties;
 @end
 
 @implementation WOTRadarViewController
@@ -22,44 +21,16 @@
     
     [super viewDidLoad];
     [self initRadarData];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
 
 - (void)initRadarData {
     
-    self.parties = @[
-                         @"SPG", @"TD", @"Heavy", @"Middle", @"Light", @"Party F",
-                         @"Party G", @"Party H", @"Party I", @"Party J", @"Party K", @"Party L",
-                         @"Party M", @"Party N", @"Party O", @"Party P", @"Party Q", @"Party R",
-                         @"Party S", @"Party T", @"Party U", @"Party V", @"Party W", @"Party X",
-                         @"Party Y", @"Party Z"
-                         ];
-
-    
-    self.options = @[
-                     @{@"key": @"toggleValues", @"label": @"Toggle Values"},
-                     @{@"key": @"toggleHighlight", @"label": @"Toggle Highlight"},
-                     @{@"key": @"toggleXLabels", @"label": @"Toggle X-Values"},
-                     @{@"key": @"toggleYLabels", @"label": @"Toggle Y-Values"},
-                     @{@"key": @"toggleRotate", @"label": @"Toggle Rotate"},
-                     @{@"key": @"toggleFill", @"label": @"Toggle Fill"},
-                     @{@"key": @"spin", @"label": @"Spin"},
-                     @{@"key": @"saveToGallery", @"label": @"Save to Camera Roll"}
-                     ];
     
     self.radarView.delegate = self;
     
@@ -85,67 +56,12 @@
     l.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:10.f];
     l.xEntrySpace = 7.0;
     l.yEntrySpace = 5.0;
-    [self setData];
 
-}
+    WOTRadarSampleMetric *sample = [[WOTRadarSampleMetric alloc] init];
+    
+    self.radarView.data = sample.chartData;
 
-- (void)setData
-{
-    double mult = 10.f;
-    int count = 5;
-    
-    NSMutableArray *yVals1 = [[NSMutableArray alloc] init];
-    NSMutableArray *yVals2 = [[NSMutableArray alloc] init];
-    NSMutableArray *yVals3 = [[NSMutableArray alloc] init];
-    NSMutableArray *yVals4 = [[NSMutableArray alloc] init];
-    NSMutableArray *yVals5 = [[NSMutableArray alloc] init];
-    
-    for (int i = 0; i < count; i++)
-    {
-        [yVals1 addObject:[[ChartDataEntry alloc] initWithValue:(arc4random_uniform(mult) + mult / 2) xIndex:i]];
-        [yVals2 addObject:[[ChartDataEntry alloc] initWithValue:(arc4random_uniform(mult) + mult / 2) xIndex:i]];
-        [yVals3 addObject:[[ChartDataEntry alloc] initWithValue:(arc4random_uniform(mult) + mult / 2) xIndex:i]];
-        [yVals4 addObject:[[ChartDataEntry alloc] initWithValue:(arc4random_uniform(mult) + mult / 2) xIndex:i]];
-        [yVals5 addObject:[[ChartDataEntry alloc] initWithValue:(arc4random_uniform(mult) + mult / 2) xIndex:i]];
-    }
-    
-    NSMutableArray *xVals = [[NSMutableArray alloc] init];
-    
-    for (int i = 0; i < count; i++)
-    {
-        [xVals addObject:self.parties[i % self.parties.count]];
-    }
-    
-    RadarChartDataSet *set5 = [[RadarChartDataSet alloc] initWithYVals:yVals5 label:@"Set 5"];
-    [set5 setColor:ChartColorTemplates.vordiplom[0]];
-    set5.drawFilledEnabled = YES;
-    set5.lineWidth = 2.0;
-    
-    RadarChartDataSet *set4 = [[RadarChartDataSet alloc] initWithYVals:yVals4 label:@"Set 4"];
-    [set4 setColor:ChartColorTemplates.vordiplom[4]];
-    set4.drawFilledEnabled = YES;
-    set4.lineWidth = 2.0;
-    
-    RadarChartDataSet *set1 = [[RadarChartDataSet alloc] initWithYVals:yVals1 label:@"Set 1"];
-    [set1 setColor:ChartColorTemplates.vordiplom[1]];
-    set1.drawFilledEnabled = YES;
-    set1.lineWidth = 2.0;
-    
-    RadarChartDataSet *set2 = [[RadarChartDataSet alloc] initWithYVals:yVals2 label:@"Set 2"];
-    [set2 setColor:ChartColorTemplates.vordiplom[2]];
-    set2.drawFilledEnabled = YES;
-    set2.lineWidth = 2.0;
 
-    RadarChartDataSet *set3 = [[RadarChartDataSet alloc] initWithYVals:yVals3 label:@"Set 3"];
-    [set3 setColor:ChartColorTemplates.vordiplom[3]];
-    set3.drawFilledEnabled = YES;
-    set3.lineWidth = 2.0;
-    
-    RadarChartData *data = [[RadarChartData alloc] initWithXVals:xVals dataSets:@[set1, set2, set3, set4, set5]];
-    [data setValueFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:8.f]];
-    [data setDrawValues:NO];
-    
-    self.radarView.data = data;
 }
 
 #pragma mark - ChartViewDelegate
