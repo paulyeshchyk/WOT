aae94e283808945da5009cac78d21e8fdf92be97
Pavel Yeshchyk <Pavel_Yeshchyk@epam.com>
2018-06-21 16:05:06 +0300

    pod deintegrate

--- WOT-iOS/Pods/Target Support Files/ASProgressPopUpView/ASProgressPopUpView-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "ASPopUpView.h"
-#import "ASProgressPopUpView.h"
-
-FOUNDATION_EXPORT double ASProgressPopUpViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char ASProgressPopUpViewVersionString[];
-
