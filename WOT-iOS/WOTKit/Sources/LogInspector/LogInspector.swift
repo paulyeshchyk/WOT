849e11396ee3e8d0434b428aa88bb70775a6d54c
Paul <paul.yestchick@gmail.com>
2022-12-18 18:29:45 +0300

    OSLogWrapper implementation

--- WOT-iOS/WOTKit/Sources/LogInspector/LogInspector.swift
+++ WOT-iOS/WOTKit/Sources/LogInspector/LogInspector.swift
@@ -10,30 +10,33 @@ import Foundation
 
 @objc
 public class LogInspector: NSObject, LogInspectorProtocol {
-    private var prioritiesToLog: [LogEventType] = []
+    private var prioritiesToLog: [LogEventType]?
+    private var output: [LOGOutputProtocol]?
 
-    public convenience init(priorities: [LogEventType]) {
+    public convenience init(priorities: [LogEventType]?, output: [LOGOutputProtocol]?) {
         self.init()
+        self.output = output
         self.prioritiesToLog = priorities
     }
 
     public func logEvent(_ event: LogEventProtocol?, sender: Any?) {
         guard  let event = event else { return }
         guard isLoggable(eventClass: type(of: event)) else { return }
-        let senderMessage: String
-        if let sender = sender {
-            senderMessage = "::\(String(describing: sender))"
-        } else {
-            senderMessage = ""
+        switch type(of: event).type {
+        case .error:
+            output?.forEach { $0.error(event.message, context: event.name) }
+        case .info:
+            output?.forEach { $0.info(event.message, context: event.name) }
+        case .warning:
+            output?.forEach { $0.warning(event.message, context: event.name) }
+        default:
+            output?.forEach { $0.debug(event.message, context: event.name) }
         }
-
-        let eventType = event.name// ?? "UNKNOWN logeventType"
-        let eventMessage = event.message// ?? ""
-
-        print("[\(eventType)\(senderMessage)]{\(eventMessage)}")
+        
     }
 
     private func isLoggable(eventClass: LogEventProtocol.Type) -> Bool {
+        guard let prioritiesToLog = prioritiesToLog else { return true }
         return prioritiesToLog.contains(eventClass.type)
     }
 }
