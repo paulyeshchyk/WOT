f31a17770cd46987852e5097be1d1faeb10c6e9b
Paul <paul.yestchick@gmail.com>
2023-01-24 16:47:05 +0300

    UOWProgress

--- WOT-iOS/ContextSDK/source/UoW/UOWManager.swift
+++ WOT-iOS/ContextSDK/source/UoW/UOWManager.swift
@@ -83,25 +83,85 @@ public class UOWManager: UOWManagerProtocol {
         }
 
         let oq = UOWOperationQueue()
-        let op = try runnable.blockOperation(forCompletion: listenerCompletion)
+        let op = try runnable.blockOperation { obj in
+            listenerCompletion(obj)
+        }
         oq.addOperation(op)
     }
 
-    var observation: NSKeyValueObservation?
     public func run(sequence: [UOWProtocol], listenerCompletion: @escaping(SequenceCompletionType)) throws {
         //
+
+        let progress = UOWProgress()
+        var completed: Bool = false
         let set = sequence.compactMap {
             return try? ($0 as? UOWRunnable)?.blockOperation { _ in
-                //
+                progress.increaseDone(by: 1) { isInProgress in
+                    if completed {
+                        assertionFailure("should not be here")
+                    }
+
+                    if !isInProgress {
+                        listenerCompletion(0, nil)
+                        completed = true
+                    }
+                }
             }
         }
+
+        progress.increaseTobeDone(by: (set.count))
+
         let sequenceOperationQueue = UOWOperationQueue()
         sequenceOperationQueue.addOperations(set, waitUntilFinished: false)
-        observation = sequenceOperationQueue.observe(\.operationCount) { operationQueue, _ in
-            if operationQueue.operationCount == 0 {
-                listenerCompletion(0, nil)
-                self.observation = nil
-            }
+    }
+}
+
+// MARK: - UOWProgress
+
+class UOWProgress {
+
+    func checkIsInProgress(completion: (Bool) -> Void) {
+        backgroundQueue.sync {
+            let result = toBeDoneCount != doneCount
+            completion(result)
+        }
+    }
+
+    // to be used with backgroundQueue
+    private var result: Double {
+        guard toBeDoneCount != 0 else { return 1 }
+        guard doneCount != 0 else { return 0 }
+        guard toBeDoneCount != doneCount else {
+            toBeDoneCount = 0
+            doneCount = 0
+            return 1
+        }
+        return Double(doneCount) / Double(toBeDoneCount)
+    }
+
+    var onValueChanged: ((Double) -> Void)?
+
+    private let backgroundQueue = DispatchQueue.init(label: "progressQueue")
+
+    private var toBeDoneCount: Int = 0
+
+    private var doneCount: Int = 0
+
+    func increaseTobeDone(by: Int = 1) {
+        backgroundQueue.sync {
+            toBeDoneCount += by
+            onValueChanged?(result)
+        }
+    }
+
+    func increaseDone(by: Int = 1, isInProgressCompletion: ((Bool) -> Void)? = nil) {
+        backgroundQueue.sync {
+            doneCount += by
+
+            let isInProgress = toBeDoneCount != doneCount
+            isInProgressCompletion?(isInProgress)
+
+            onValueChanged?(result)
         }
     }
 }
